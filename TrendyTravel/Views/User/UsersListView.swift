c25f0e28b93cbd87386e2c24fde509c8963b8278
Cynthia-Cyber-Code <cynthiaf181999@gmail.com>
2023-07-18 09:45:13 +0200

    Add navigation All Creators

---
+++ TrendyTravel/Views/User/UsersListView.swift
@@ -0,0 +1,42 @@
+//
+//  UsersListView.swift
+//  TrendyTravel
+//
+//  Created by Cynthia on 17/07/2023.
+//
+
+import SwiftUI
+
+struct UsersListView: View {
+    @ObservedObject var vm: UserViewModel
+    let column = [GridItem(.flexible()), GridItem(.flexible())]
+    var body: some View {
+        ScrollView {
+            LazyVGrid(columns: column) {
+                ForEach(vm.users, id: \.self) { user in
+                    NavigationLink {
+                        UserDetailsView(user: user)
+                    } label: {
+                        DiscoverUserView(user: user)
+                    }
+                    
+                }
+            }
+            .onAppear {
+                vm.users = vm.getUsers()
+            }
+            .padding(.horizontal)
+            .padding(.bottom)
+        }
+        .navigationTitle("All creators")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct UsersListView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            UsersListView(vm: UserViewModel())
+        }
+    }
+}
