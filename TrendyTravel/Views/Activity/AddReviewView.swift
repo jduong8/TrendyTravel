afe20442a4ccee4a071c62e20d8ecf7bcf4c69db
Razafindratsira Ny Dina <nydina.razafindratsira@legrand.com>
2023-07-20 16:26:15 +0200

    Create the view for add a review

---
+++ TrendyTravel/Views/Activity/AddReviewView.swift
@@ -0,0 +1,70 @@
+//
+//  AddReviewView.swift
+//  TrendyTravel
+//
+//  Created by Dina RAZAFINDRATSIRA on 20/07/2023.
+//
+
+import SwiftUI
+
+struct AddReviewView: View {
+    @State private var userRating = 0
+    @State private var comment: String = ""
+    
+    var body: some View {
+        VStack (spacing: 25){
+            Text("How would you rate your experience?")
+                .font(.title3)
+                .foregroundColor(.secondary)
+            
+            StarRatingView(rating: $userRating)
+            //                Text("Your rating: \(userRating) stars")
+            
+            TextField(
+                "Add your comment here..",
+                text: $comment
+            )
+            .padding()
+            .background(Color.gray.opacity(0.2))
+            .cornerRadius(10)
+            
+            Button {
+                // TODO: Handle the button action here
+            } label: {
+                Text("Add a review")
+                    .padding(.horizontal)
+            }
+            .buttonStyle(.borderedProminent)
+            .accentColor(.cyan)
+
+
+        }
+        .padding()
+    }
+}
+
+struct AddReviewView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddReviewView()
+    }
+}
+
+
+struct StarRatingView: View {
+    @Binding var rating: Int
+    
+    var body: some View {
+        HStack(spacing: 2) {
+            ForEach(0..<5) { index in
+                Button(action: {
+                    rating = index + 1
+                }) {
+                    Image(systemName: index < rating ? "star.fill" : "star")
+                        .foregroundColor(index < rating ? .yellow : .gray)
+                        .font(.title)
+                }
+            }
+        }
+    }
+}
+
