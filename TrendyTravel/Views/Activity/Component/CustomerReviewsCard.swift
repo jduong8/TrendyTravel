d576ebaacc0ef411f4a94c5188fc94abd93054b1
Tanya Simmer <101064028+tatkagore@users.noreply.github.com>
2023-07-20 15:37:03 +0200

    Popular Activities - Customers Reviews

--- TrendyTravel/Views/Activity/Component/CustomerReviewsCard.swift
+++ TrendyTravel/Views/Activity/Component/CustomerReviewsCard.swift
@@ -9,7 +9,7 @@
 import SwiftUI
 
 struct ActivityReviewsView: View {
-
+    
     @ObservedObject var reviewViewModel: ActivityReviewViewModel
     
     var body: some View {
@@ -18,12 +18,37 @@ struct ActivityReviewsView: View {
                 .font(.system(size: 16, weight: .bold))
             ForEach(reviewViewModel.review, id: \.id) { review in
                 VStack(alignment: .leading, spacing: 4) {
-                    StarRatingView(rating: review.rating)
-                        .foregroundColor(.orange)
-                    Text("\(review.content)")
-                    if let user = review.user {
-                        Text("\(user.firstName) \(user.lastName)")
+                    HStack {
+                        AsyncImage(
+                            url: URL(string: review.user?.profilImage ?? ""),
+                            content: { image in
+                                image
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 44)
+                                    .clipShape(Circle())
+                            },
+                            placeholder: {
+                                Image(systemName: "person.circle.fill")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 44)
+                                    .clipShape(Circle())
+                            }
+                        )
+                        VStack(alignment: .leading, spacing: 4) {
+                            if let user = review.user {
+                                Text("\(user.firstName) \(user.lastName)")
+                                    .font(.system(size: 14,weight: .bold))
+                            }
+                            StarRatingView(rating: review.rating)
+                                .foregroundColor(.orange)
+                        }
+                        Spacer()
+                        Text(review.formattedCreatedAt)
+                            .font(.system(size: 14, weight: .bold))
                     }
+                    Text("\(review.content)")
                 }
                 .padding()
                 .background(Color.white.opacity(0.1))
@@ -37,37 +62,3 @@ struct ActivityReviewsView: View {
         .padding(.horizontal)
     }
 }
-
-//LazyVStack(alignment: .leading, spacing: 8) {
-//    Text("Customer Reviews")
-//        .font(.system(size: 16, weight: .bold))
-//    ForEach(reviewViewModel.review, id: \.id) { review in
-//
-//        VStack(alignment: .leading, spacing: 4) {
-//            ForEach(userViewModel.users, id: \.self) { user in
-//                if review.userID == user.id {
-//                    Text(user.firstName)
-//
-//                }
-//            }
-//            .onAppear{
-//                Task {
-//                    userViewModel.users = try await userViewModel.getUsers()
-//                }
-//            }
-//            StarRatingView(rating: review.rating)
-//                .foregroundColor(.orange)
-//            Text("\(review.content)")
-//
-//        }
-//        .padding()
-//        .background(Color.white.opacity(0.1))
-//        .cornerRadius(8)
-//        .overlay(
-//            RoundedRectangle(cornerRadius: 8)
-//                .stroke(Color.gray.opacity(0.5), lineWidth: 1)
-//        )
-//    }
-//
-//}
-//.padding(.horizontal)
