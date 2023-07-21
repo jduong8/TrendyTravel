6c97253cdf902f2ab8565b7163b80f42a52ee5b0
Razafindratsira Ny Dina <nydina.razafindratsira@legrand.com>
2023-07-21 11:37:37 +0200

    Add a new Review Button, add a navigation

--- TrendyTravel/Views/Activity/ActivityDetailsView.swift
+++ TrendyTravel/Views/Activity/ActivityDetailsView.swift
@@ -8,68 +8,82 @@
 import SwiftUI
 
 struct ActivityDetailsView: View {
-
+    
     @StateObject private var reviewViewModel: ActivityReviewViewModel
-
+    @State private var isShowingAddReviewSheet = false
+    
     init(selectedActivity: Activity) {
         self._reviewViewModel = StateObject(wrappedValue: ActivityReviewViewModel(selectedActivity: selectedActivity))
     }
-
+    
     var body: some View {
-        ScrollView {
-            ZStack(alignment: .bottomLeading) {
-                AsyncImage(
-                    url: URL(string: reviewViewModel.selectedActivity.imageName),
-                    content: { image in
-                        image
-                            .resizable()
-                            .frame(height: 200)
-                    },
-                    placeholder: {
-                        ProgressView()
-                    }
-                )
-                LinearGradient(gradient: Gradient(colors: [Color.clear, Color.black]), startPoint: .center, endPoint: .bottom)
-                
-                HStack {
-                    VStack(alignment: .leading, spacing: 4) {
-                        Text(reviewViewModel.selectedActivity.name)
-                            .foregroundColor(.white)
-                            .font(.system(size: 18, weight: .bold))
-                        HStack {
-                            StarRatingView(rating: reviewViewModel.selectedActivity.rating)
-                                .font(.title2)
-                                .foregroundColor(.orange)
-                        }
-                    }
-                    Spacer()
-                }
-                .padding()
-            }
-            VStack(alignment: .leading, spacing: 8) {
-                Text("Location & Description")
-                    .font(.system(size: 16, weight: .bold))
-                if let activity = reviewViewModel.selectedActivity.destination {
-                    Text("\(activity.city.capitalized), \(activity.country.capitalized)")
-                }
-                HStack {
-                    Text(reviewViewModel.selectedActivity.price)
-                }
-                .foregroundColor(.orange)
-                HStack { Spacer()}
-            }
-            .padding(.top)
-            .padding(.horizontal)
-            Text(reviewViewModel.selectedActivity.description)
-                .padding(.top, 8)
-                .font(.system(size: 14, weight: .regular))
-                .padding(.horizontal)
-                .padding(.bottom)
-            Divider()
-                .padding(.horizontal)
+        VStack {
             ScrollView {
-                ActivityReviewsView(reviewViewModel: reviewViewModel)
+                ZStack(alignment: .bottomLeading) {
+                    AsyncImage(
+                        url: URL(string: reviewViewModel.selectedActivity.imageName),
+                        content: { image in
+                            image
+                                .resizable()
+                                .frame(height: 200)
+                        },
+                        placeholder: {
+                            ProgressView()
+                        }
+                    )
+                    LinearGradient(gradient: Gradient(colors: [Color.clear, Color.black]), startPoint: .center, endPoint: .bottom)
+                    
+                    HStack {
+                        VStack(alignment: .leading, spacing: 4) {
+                            Text(reviewViewModel.selectedActivity.name)
+                                .foregroundColor(.white)
+                                .font(.system(size: 18, weight: .bold))
+                            HStack {
+                                StarRatingView(rating: reviewViewModel.selectedActivity.rating)
+                                    .font(.title2)
+                                    .foregroundColor(.orange)
+                            }
+                        }
+                        Spacer()
+                    }
+                    .padding()
+                }
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("Location & Description")
+                        .font(.system(size: 16, weight: .bold))
+                    if let activity = reviewViewModel.selectedActivity.destination {
+                        Text("\(activity.city.capitalized), \(activity.country.capitalized)")
+                    }
+                    HStack {
+                        Text(reviewViewModel.selectedActivity.price)
+                    }
+                    .foregroundColor(.orange)
+                    HStack { Spacer()}
+                }
+                .padding(.top)
+                .padding(.horizontal)
+                Text(reviewViewModel.selectedActivity.description)
+                    .padding(.top, 8)
+                    .font(.system(size: 14, weight: .regular))
+                    .padding(.horizontal)
+                    .padding(.bottom)
+                Divider()
+                    .padding(.horizontal)
+                ScrollView {
+                    ActivityReviewsView(reviewViewModel: reviewViewModel)
+                }
+                
             }
+            ShowAddReview()
+                .onTapGesture {
+                    isShowingAddReviewSheet = true
+                }
+                .sheet(isPresented: $isShowingAddReviewSheet) {
+                    AddReviewView()
+                        .presentationDetents([.medium])
+                }
+            
+            
         }
         .navigationBarTitle("Activity Details", displayMode: .inline)
     }
